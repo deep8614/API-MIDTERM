f6b6a66eea95a1a1a604038ad73d47f3740d9250
deep8614 <deep8614>
2024-10-18 17:21:19 -0400

    adding load user method withmongodb

--- server.js
+++ server.js
@@ -19,3 +19,25 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+const User = mongoose.model('User', userSchema);
+
+// Load users.json and insert into MongoDb
+const loadUsers = async () => {
+  const usersFilePath = path.join(__dirname, 'users.json');
+  const usersData = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+  const existingUsers = await User.find({});
+  if (existingUsers.length === 0) {
+    await User.insertMany(usersData);
+    console.log('Users loaded into MongoDB');
+  } else {
+    console.log('Users already exist in the database');
+  }
+};
+
+// Call function to load users on server startup
+loadUsers();
+
+// Middleware to parse JSON
+app.use(express.json());
+
